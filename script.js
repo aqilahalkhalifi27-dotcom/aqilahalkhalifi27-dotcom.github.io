94d95c26488b12c9565a8e4bf0405298b331f273
aqilahalkhalifi27-dotcom <aqilah.alkhalifi27@sma.belajar.id>
2025-08-13 09:45:29 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function filterMochi(category) {
-  const cards = document.querySelectorAll('.card');
-  cards.forEach(card => {
-    const isMatch = category === 'all' || card.dataset.category === category;
-    card.style.display = isMatch ? 'block' : 'none';
-  });
-}
